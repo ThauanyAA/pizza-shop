7988244d85a3b3b0c91fc6ca71e5f5894cc34aa3
thauanyASN <thauanya@trustsecurenow.com>
2025-07-01 18:00:54 -0300

    Adding theme toggle and account menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
       <Toaster richColors />
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   )
 }
