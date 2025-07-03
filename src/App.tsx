1a94c94f904b9a06f3c12155bf51c99196781ad3
ThauanyAA <thauanyaraujo5@gmail.com>
2025-07-03 17:51:10 -0300

    Set up conection with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './index.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
   return (
     <ThemeProvider storageKey="pizzashop-theme" defaultTheme="system">
       <Toaster richColors />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
