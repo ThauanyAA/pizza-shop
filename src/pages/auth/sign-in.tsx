8c62594e3927b86da125e8ef5ddc2ba5b4f310ac
ThauanyAA <thauanyaraujo5@gmail.com>
2025-07-23 18:16:18 -0300

    Fix bug with input on signin age

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -9,20 +9,19 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-const signInForm = z.object({
-  email: z.string().email(),
+const signInFormSchema = z.object({
+  email: z.string().email('E-mail inválido'),
 })
 
-type SignInForm = z.infer<typeof signInForm>
+type SignInFormData = z.infer<typeof signInFormSchema>
 
 export function SignIn() {
   const [searchParams] = useSearchParams()
-
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>({
+  } = useForm<SignInFormData>({
     defaultValues: {
       email: searchParams.get('email') ?? '',
     },
@@ -32,27 +31,31 @@ export function SignIn() {
     mutationFn: signIn,
   })
 
-  async function handleSignIn(data: SignInForm) {
+  async function handleSignIn(data: SignInFormData) {
     try {
       await authenticate({ email: data.email })
 
-      toast.success('Enviamos um link de autenticação para seu e-mail.', {
-        action: {
-          label: 'Reenviar',
-          onClick: () => {
-            handleSignIn(data)
+      toast.success(
+        'Enviamos um e-mail com as instruções para acessar o painel',
+        {
+          action: {
+            label: 'Reenviar e-mail',
+            onClick: () => {
+              handleSignIn(data)
+            },
           },
         },
-      })
+      )
     } catch (error) {
-      toast.error('Credenciais inválidas.')
+      console.error(error)
+      toast.error('Credenciais inválidas')
     }
   }
 
   return (
     <div className="p-8">
-      <Button variant="ghost" asChild className="absolute right-8 top-8">
-        <Link to="/sign-up">Novo estabelecimento</Link>
+      <Button variant="ghost" asChild className="absolute top-8 right-8">
+        <Link to="/sign-up">Novo Estabelecimento</Link>
       </Button>
 
       <div className="flex w-[350px] flex-col justify-center gap-6">
@@ -60,7 +63,7 @@ export function SignIn() {
           <h1 className="text-2xl font-semibold tracking-tight">
             Acessar painel
           </h1>
-          <p className="text-sm text-muted-foreground">
+          <p className="text-muted-foreground text-sm">
             Acompanhe suas vendas pelo painel do parceiro!
           </p>
         </div>
@@ -68,7 +71,7 @@ export function SignIn() {
         <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
           <div className="space-y-2">
             <Label htmlFor="email">Seu e-mail</Label>
-            <Input data-testid="email" id="email" type="email" {...register('email')} />
+            <Input id="email" type="email" {...register('email')} />
           </div>
 
           <Button disabled={isSubmitting} className="w-full" type="submit">
