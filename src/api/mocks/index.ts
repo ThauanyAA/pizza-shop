5b21aa743beb927db140e14e81f4d6b7f9d37428
ThauanyAA <thauanyaraujo5@gmail.com>
2025-07-30 17:38:35 -0300

    Create Profile and Managed Restaurant Mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,14 +1,18 @@
 import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
-import { signInMock } from './sign-in-mock'
-import { registerRestaurantMock } from './register-restaurant-mock'
-import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
-import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
-import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
-import { getMonthRevenueMock } from './get-month-revenue-mock'
+
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
+import { getMonthRevenueMock } from './get-month-revenue-mock'
 import { getPopularProductsMock } from './get-poular-products-mock'
+import { getProfileMock } from './get-profile-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
+import { signInMock } from './sign-in-mock'
+import { updateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
   signInMock,
@@ -19,6 +23,9 @@ export const worker = setupWorker(
   getMonthRevenueMock,
   getDailyRevenueInPeriodMock,
   getPopularProductsMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock,
 )
 
 export async function enableMSW() {
