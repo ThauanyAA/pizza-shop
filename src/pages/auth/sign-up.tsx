65b68990dc6cc9966e4ed6ea20d46e06a19ebb10
ThauanyAA <thauanyaraujo5@gmail.com>
2025-08-02 15:19:05 -0300

    E2E Tests - Sign In and Sign Up

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -49,14 +49,14 @@ export function SignUp() {
       })
     } catch (error) {
       console.error(error)
-      toast.error('Erro ao tentar cadastrar o restaurante')
+      toast.error('Erro ao cadastrar o restaurante')
     }
   }
 
   return (
     <div className="p-8">
       <Button variant="ghost" asChild className="absolute top-8 right-8">
-        <Link to="/sign-up">Novo Estabelecimento</Link>
+        <Link to="/sign-in">Fazer Login</Link>
       </Button>
       <div className="flex w-[350px] flex-col justify-center gap-6">
         <div className="flex flex-col gap-2 text-center">
